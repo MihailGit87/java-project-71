4e8d4087ac7fc0a7178eea84abf536ba225d5c6b
mikhailvasilev <mall.87@mail.ru>
2023-08-21 13:44:17 +0300

    change App(picocli)

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,37 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1",
+        description = "Compares two configuration files and shows a difference.")
+
+public class App implements Callable<Integer> {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+    @Parameters(paramLabel = "filepath1", index = "0", description = "path to first file")
+    private String firstFilePath;
+
+    @Parameters(paramLabel = "filepath2", index = "1", description = "path to second file")
+    private String secondFilePath;
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format",
+            description = "output format [default: stylish]", defaultValue = "stylish")
+    private String format;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    boolean versionInfoRequested;
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    boolean usageHelpRequested;
+    @Override
+    public Integer call() throws Exception {
+        return null;
     }
 }
