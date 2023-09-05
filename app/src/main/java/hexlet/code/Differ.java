c97a1d913dd17e7e37173673d5fb03a2a2951e7b
mikhailvasilev <mall.87@mail.ru>
2023-09-05 10:55:20 +0300

    add something

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,8 +1,16 @@
 package hexlet.code;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
 public class Differ {
-    public static String generate () {
-        String diff = "";
+    public static String generate (String file1, String file2) throws IOException {
+
+
+        //        Map<String, Object> firstFileToMap = Files.readString(Path.of(file1));
+
         return diff;
     }
 }
