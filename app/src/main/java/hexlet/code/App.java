baf454e17b58b048f6d0c70e12cd9749793d9b56
mikhailvasilev <mall.87@mail.ru>
2023-09-18 17:57:53 +0300

    add something

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,21 +5,19 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.io.File;
+import java.nio.file.Files;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1",
         description = "Compares two configuration files and shows a difference.")
 
-public class App implements Callable<Integer> {
-    public static void main(String[] args) {
-        int exitCode = new CommandLine(new App()).execute(args);
-        System.exit(exitCode);
-    }
-    @Parameters(paramLabel = "filepath1", index = "0", description = "path to first file")
-    private String firstFilePath;
+public class App implements Callable<String> {
 
-    @Parameters(paramLabel = "filepath2", index = "1", description = "path to second file")
-    private String secondFilePath;
+    @Parameters(index = "0", description = "path to first file")
+    private File filepath1 = new File("app/src/main/java/hexlet/code/file1.json");
+    @Parameters(index = "1", description = "path to second file")
+    private File filepath2 = new File("app/src/main/java/hexlet/code/file2.json");
 
     @Option(names = {"-f", "--format"}, paramLabel = "format",
             description = "output format [default: stylish]", defaultValue = "stylish")
@@ -30,8 +28,20 @@ public class App implements Callable<Integer> {
 
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
     boolean usageHelpRequested;
+
     @Override
-    public Integer call() throws Exception {
-        return null;
+    public String call() throws Exception {
+
+        if (Files.exists(filepath1.toPath())){
+            throw new Exception("File" + filepath1.getName() + "does not exist");
+        }
+        if (Files.exists(filepath2.toPath())){
+            throw new Exception("File" + filepath2.getName() + "does not exist");
+        }
+        return Differ.generate(filepath1, filepath2);
+    }
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,16 +1,51 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Differ {
-    public static String generate (String file1, String file2) throws IOException {
+    public static String generate(File file1, File file2) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> parsMap1 = mapper.readValue(file1, new TypeReference<Map<String, Object>>() {
+        });
+        Map<String, Object> parsMap2 = mapper.readValue(file2, new TypeReference<Map<String, Object>>() {
+        });
 
+        Map<String, Object> commonMap = new HashMap<>();
+        commonMap.putAll(parsMap1);
+        commonMap.putAll(parsMap2);
 
-        //        Map<String, Object> firstFileToMap = Files.readString(Path.of(file1));
-
-        return diff;
+        String  result;
+        result = commonMap
+                .keySet()
+                .stream()
+                .sorted()
+                .map(key -> {
+                    String out = "";
+                    if (parsMap1.containsKey(key) && !parsMap2.containsKey(key)){
+                        out = out + "-" + key + ": " + parsMap1.get(key);
+                    } else if (!parsMap1.containsKey(key) && parsMap2.containsKey(key)) {
+                        out = out + "+ " + key + ": " + parsMap2.get(key);
+                    } else {
+                        if(parsMap1.get(key).equals(parsMap2.get(key))){
+                            out = out + " " + key + parsMap1.get(key);
+                        } else {
+                            out = out + "- " + key + ": " + parsMap1.get(key) + "\n";
+                            out = out + "+ " + key + ": " + parsMap2.get(key);
+                        }
+                    }
+                    return out;
+                })
+                .collect(Collectors.joining("\n"));
+        System.out.println(result);
+        return result;
     }
 }
