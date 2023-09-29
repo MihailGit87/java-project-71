2e9a4d6d8576208e68b3721b71e4440d05de9fbc
mikhailvasilev <mall.87@mail.ru>
2023-09-29 22:32:48 +0300

    update test

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -10,18 +10,7 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTest {
-    File file1;
-    File file2;
-    @BeforeEach
-    public void setup() {
-        file1 = new File("/home/mikhailvasilev/java-project-71/app/src/test/resources/filepath1.json");
-        file2 = new File("/home/mikhailvasilev/java-project-71/app/src/test/resources/filepath2.json");
-    }
-    @Test
-    public void existFile() {
-        Assertions.assertTrue(file1.exists());
-        Assertions.assertTrue(file2.exists());
-    }
+
     @Test
     void testDiffer() {
         Differ dif = new Differ();
