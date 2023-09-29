e77b8267a5e01514bf627079bf97724d31273551
mikhailvasilev <mall.87@mail.ru>
2023-09-29 22:35:24 +0300

    update test

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -1,11 +1,11 @@
 import hexlet.code.Differ;
 //import hexlet.code.Parser;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.IOException;
+//import java.io.File;
+//import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
