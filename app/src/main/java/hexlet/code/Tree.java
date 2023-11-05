aee43c8d991cac43d1fe75f3378c595969364bba
Васильев Михаил <126636366+MihailGit87@users.noreply.github.com>
2023-11-05 10:55:59 +0300

    Update Tree.java

--- app/src/main/java/hexlet/code/Tree.java
+++ app/src/main/java/hexlet/code/Tree.java
@@ -1,33 +1,43 @@
 package hexlet.code;
 
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Stream;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Tree {
     public static List<Map<String, Object>> build(Map<String, Object> firstData, Map<String, Object> secondData) {
-        return Stream.concat(firstData.keySet().stream(), secondData.keySet().stream())
-                .distinct()
-                .sorted(Comparator.comparing(String::format))
-                .map(key -> {
-                    Object value1 = Optional.ofNullable(firstData.get(key)).orElse("null");
-                    Object value2 = Optional.ofNullable(secondData.get(key)).orElse("null");
 
-                    if (firstData.containsKey(key) && secondData.containsKey(key)) {
-                        if (Objects.equals(value1, value2)) {
-                            return Map.of("field", key, "status", "unchanged", "value", value1);
-                        } else {
-                            return Map.of("field", key, "status", "changed", "value1", value1,
-                                    "value2", value2);
-                        }
-                    } else if (secondData.containsKey(key)) {
-                        return Map.of("field", key, "status", "added", "value2", value2);
-                    } else {
-                        return Map.of("field", key, "status", "removed", "value1", value1);
-                    }
-                }).toList();
+        List<Map<String, Object>> result = new ArrayList<>();
+        Set<Object> keys = new TreeSet<>(firstData.keySet());
+        keys.addAll(secondData.keySet());
+
+        for (Object key: keys) {
+            Map<String, Object> diff = new LinkedHashMap<>();
+
+            if (!firstData.containsKey(key)) {
+                diff.put("status", "added");
+                diff.put("field", key);
+                diff.put("value2", secondData.get(key));
+            } else if (!secondData.containsKey(key)) {
+                diff.put("status", "removed");
+                diff.put("field", key);
+                diff.put("value1", firstData.get(key));
+            } else if (Objects.equals(firstData.get(key), secondData.get(key))) {
+                diff.put("status", "unchanged");
+                diff.put("field", key);
+                diff.put("value", firstData.get(key));
+            } else {
+                diff.put("status", "changed");
+                diff.put("field", key);
+                diff.put("value2", secondData.get(key));
+                diff.put("value1", firstData.get(key));
+            }
+            result.add(diff);
+        }
+        return result;
     }
 }
